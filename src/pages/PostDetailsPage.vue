4292ccf587c62ff4d11d6f283834707b635e6700
ethanvachon <ethanhvachon@gmail.com>
2021-01-24 19:52:22 -0700

    deatils page

--- src/pages/PostDetailsPage.vue
+++ src/pages/PostDetailsPage.vue
@@ -1,15 +1,23 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12 text-center">
+      <div class="col-12 d-flex justify-content-center">
         <div v-if="post">
           <div class="card">
+            <div class="d-flex border-bottom pb-3 align-items-center justify-content-center">
+              <img :src="post.creator.picture" class="card-img-top">
+              <h1 class="pl-2">{{ post.creator.name }}</h1>
+            </div>
             <div class="card-body">
-              <h4 class="card-title">{{ post.title }}</h4>
-              <p class="card-text">{{ post.body }}</p>
+              <div class="text-center">
+                <h1 class="card-title">{{ post.title }}</h1>
+                <h4 class="card-text pb-5">{{ post.body }}</h4>
+              </div>
               <div v-for="comment in state.comments" :key="comment.id">
-                <p>{{ comment.creator.name }}</p>
-                <p>{{ comment.body }}</p>
+                <div class="d-flex pt-2 border-top width">
+                  <p class="pr-2 border-right bold">{{ comment.creator.name }}</p>
+                  <p class="pl-2">{{ comment.body }}</p>
+                </div>
               </div>
             </div>
           </div>
@@ -41,12 +49,23 @@ export default {
       }
     })
     return {
-      post: computed(() => AppState.selected)
+      post: computed(() => AppState.selected),
+      state
     }
   }
 }
 </script>
 
 <style scoped>
-
+  .card{
+    width: 70vw;
+  }
+  .width{
+    width: 67vw;
+    overflow: auto;
+  }
+  img{
+    width: 7vw;
+    height: auto;
+  }
 </style>
